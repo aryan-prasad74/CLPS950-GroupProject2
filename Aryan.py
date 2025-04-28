7079650ce39aec92d6e0d82921e48fd388823508
aryan-prasad74 <aryan_prasad@brown.edu>
2025-04-28 17:22:40 -0400

    Trying to extract top artist from personal account

--- Aryan.py
+++ Aryan.py
@@ -1,12 +1,22 @@
 
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="a3596ff7cab74bbf870820453fc22d5e",
-                                                           client_secret="589446335f0c4a29a3b894b364826006"))
+# Set your credentials and redirect URI
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id="a3596ff7cab74bbf870820453fc22d5e",
+    client_secret="589446335f0c4a29a3b894b364826006",
+    redirect_uri="http://127.0.0.1:8888/callback",
+    scope=["user-top-read"]
+))
 
-results = sp.search(q='Drake', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
-
-    print('hello')
+# Fetch the current user's top artists
+top_artists = sp.current_user_top_artists(limit=5, time_range='medium_term')
+print(top_artists)
+# top_artist = top_artists['items'][0]
+# print(f"Your top artist is {top_artist['name']}")
+# if top_artists['items']:
+#     top_artist = top_artists['items'][0]
+#     print(f"Your top artist is {top_artist['name']}")
+# else:
+#     print("No top artist data available.")
