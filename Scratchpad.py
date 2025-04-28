a7a5d7c1cfdf4d4adb5079930fc3100a971a40c2
daneishaa <daneisha_rodriguez@brown.edu>
2025-04-28 16:23:54 -0400

    weezer

--- Scratchpad.py
+++ Scratchpad.py
@@ -1,6 +1,11 @@
 ## Use this for any scratch work ##
 
-print('hello world')
-
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="YOUR_APP_CLIENT_ID",
+                                                           client_secret="YOUR_APP_CLIENT_SECRET"))
+
+results = sp.search(q='weezer', limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx, track['name'])
