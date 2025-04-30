74a790b8d28773c4c825e3ac0902f41ee4aed4df
aryan-prasad74 <aryan_prasad@brown.edu>
2025-04-30 15:20:51 -0400

    testing playlists

--- Aryan.py
+++ Aryan.py
@@ -7,7 +7,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id="95a3dd3dd0b241709a938b502eb7326a",
     client_secret="dc98f98db12d43149e4e6247a7a2fc08",
     redirect_uri="http://127.0.0.1:8888/callback",
-    scope= "user-top-read"
+    scope= "user-top-read" "playlist-read-private"
 ))
 
 ######################## JSON Test ###############################
@@ -16,7 +16,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 # print(json.dumps(response, indent=2))
 ##################################################################
 
-# ## Version 2
+# ## List playlists
 # playlists = []
 # results = sp.current_user_playlists()
 # while results:
@@ -29,28 +29,15 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 # for idx, playlist in enumerate(playlists, 1):
 #     print(f"{idx}. {playlist['name']}")
 
-# ## Version 1
-# playlists = sp.current_user_playlists()
-# print(playlists)
-# for idx, playlist in enumerate(playlists['items'], 1):
-#     print(f"{idx}. {playlist['name']}")
 
 
-##################################TOP ARTISTS################################################## (user-top-read)
-#Find top artists
-top_artists = sp.current_user_top_artists(limit=10, time_range='medium_term')
+# #Find top artists
+# top_artists = sp.current_user_top_artists(limit=1, time_range='short_term')
 
-# Check if there are any top artists, if so display, else return msg.
-if top_artists['items']:
-    for idx, artist in enumerate(top_artists['items'], 1):
-        print(f"{idx}. {artist['name']}")
-else:
-    print("No top artists found.")
+# # Check if there are any top artists, if so display, else return msg.
+# if top_artists['items']:
+#     for idx, artist in enumerate(top_artists['items'], 1):
+#         print(f"{idx}. {artist['name']}")
+# else:
+#     print("No top artists found.")
 #############################################################################################
-
-
-## To Test for 200 OK code ##
-    # import requests
-    # headers = {"Authorization": "Bearer " + sp.auth_manager.get_cached_token()['access_token']}
-    # r = requests.get("https://api.spotify.com/v1/me/top/artists?limit=10", headers=headers)
-    # print(r.status_code, r.json())
