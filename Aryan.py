d2fd01b032e7adbe77d036899d64cb2cf1ee7cc0
aryan-prasad74 <aryan_prasad@brown.edu>
2025-04-30 15:33:31 -0400

    testing both playlists and artists

--- Aryan.py
+++ Aryan.py
@@ -7,7 +7,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id="95a3dd3dd0b241709a938b502eb7326a",
     client_secret="dc98f98db12d43149e4e6247a7a2fc08",
     redirect_uri="http://127.0.0.1:8888/callback",
-    scope= "playlist-read-private" #"user-top-read"
+    scope= "playlist-read-private user-top-read"
 ))
 
 ######################## JSON Test ###############################
@@ -25,13 +25,12 @@ if playlists['items']:
         print('No Playlists Found')
 
 
-# #Find top artists
-# top_artists = sp.current_user_top_artists(limit=1, time_range='short_term')
+#Find top artists
+top_artists = sp.current_user_top_artists(limit=10, time_range='medium_term')
 
-# # Check if there are any top artists, if so display, else return msg.
-# if top_artists['items']:
-#     for idx, artist in enumerate(top_artists['items'], 1):
-#         print(f"{idx}. {artist['name']}")
-# else:
-#     print("No top artists found.")
-#############################################################################################
+# Check if there are any top artists, if so display, else return msg.
+if top_artists['items']:
+    for idx, artist in enumerate(top_artists['items'], 1):
+        print(f"{idx}. {artist['name']}")
+else:
+    print("No top artists found.")
