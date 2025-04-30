f23621fb172903ad528ca9ea052af9a5f73939e4
Jamin107 <jamin_gogo_peters@brown.edu>
2025-04-30 15:40:13 -0400

    test jamin account

--- Scratchpad.py
+++ Scratchpad.py
@@ -1,33 +1,35 @@
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# Set your credentials and redirect URI --- TEST 2
+# Set your credentials and redirect URI --- TEST(G)
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="3bd1abd2ffd2463a8a86a7bf77b6e113",
-    client_secret="0f365761c29d4fe5a595b0dd4814dfe9",
+    #client_id="95a3dd3dd0b241709a938b502eb7326a",
+    #client_secret="dc98f98db12d43149e4e6247a7a2fc08",
+    client_id="a43a77e0821246f0a5cde04e9f6a35db",
+    client_secret="0c1e99a30f0a4cc28b41220eaa8d0711",
     redirect_uri="http://127.0.0.1:8888/callback",
-    scope=["user-top-read"]
+    scope= "playlist-read-private user-top-read"
 ))
-print(sp.auth_manager.get_cached_token())
-print(sp.current_user())  
 
-# Fetch the current user's top artists
+######################## JSON Test ###############################
+# import json
+# response = sp.current_user_top_tracks(limit=10)
+# print(json.dumps(response, indent=2))
+##################################################################
+
+## List playlists
+playlists = sp.current_user_playlists()
+for idx, playlist in enumerate(playlists['items'], start=1):
+    print(f"{idx}. {playlist['name']} (ID: {playlist['id']})")
+
+
+#Find top artists
 top_artists = sp.current_user_top_artists(limit=10, time_range='medium_term')
-print(top_artists)
 
-## Rest of top artist display code ##
-# top_artist = top_artists['items'][0]
-# print(f"Your top artist is {top_artist['name']}")
-# if top_artists['items']:
-#     top_artist = top_artists['items'][0]
-#     print(f"Your top artist is {top_artist['name']}")
-# else:
-#     print("No top artist data available.")
-
-## To Test for 200 OK code ##
-    # import requests
-    # headers = {"Authorization": "Bearer " + sp.auth_manager.get_cached_token()['access_token']}
-    # r = requests.get("https://api.spotify.com/v1/me/top/artists?limit=10", headers=headers)
-    # print(r.status_code, r.json())
-
-    
+# Check if there are any top artists, if so display, else return msg.
+if top_artists['items']:
+    for idx, artist in enumerate(top_artists['items'], 1):
+        print(f"{idx}. {artist['name']}")
+else:
+    print("No top artists found.")
