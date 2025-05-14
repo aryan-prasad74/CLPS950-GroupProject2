4072e37bc215b5ea38bdbb8acd197ece222ca8f3
aryan-prasad74 <aryan_prasad@brown.edu>
2025-05-14 10:06:22 +0800

    update

--- MainProgram.py
+++ MainProgram.py
@@ -21,8 +21,26 @@ analyzed_tracks, compound_scores = analyze_tracks()
 # for idx, playlist in enumerate(playlists['items'], start=1):
 #     print(f"{idx}. {playlist['name']} (ID: {playlist['id']})")
 
-#run diagrams
-print("\nRunning Analytic Diagrams ...")
-plot_sentiment_scores(analyzed_tracks, compound_scores)
+# #run diagrams
+# print("\nRunning Analytic Diagrams ...")
+# plot_sentiment_scores(analyzed_tracks, compound_scores)
+
+sns.set(style = "whitegrid") 
+fig, axs = plt.subplots(2, 1, figsize = (12, 6))
+
+#Bar Plot- visualizes sentiment scores per song
+sns.barplot(x = compound_scores, y = analyzed_tracks, ax = axs[0], palette = "coolwarm")
+axs[0].set_title("Sentiment Scores per Song")
+axs[0].set_ylabel("Compound Score")
+axs[0].set_xlabel("Tracks")
+
+#Historgram - visualizes distribution of sentiment scores
+sns.histplot(compound_scores, bins = 20, kde = True, ax = axs[1], color = 'skyblue')
+axs[1].set_title("Distribution of Sentiment Scores")
+axs[1].set_ylabel("Compound Score")
+axs[1].set_xlabel("Frequency")
+
+plt.tight_layout()
+plt.show()
 
 print("\nComplete! Thanks for using our program.")
